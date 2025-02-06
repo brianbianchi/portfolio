a692ef40f84d4c134b34c1d956df7ae082231b1e
brianbianchi <bianchibrianj@gmail.com>
2025-02-05 23:18:43 -0500

    static files

--- core/settings.py
+++ core/settings.py
@@ -162,6 +162,8 @@ SECURE_CROSS_ORIGIN_OPENER_POLICY = None
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+# collectstatic location
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
