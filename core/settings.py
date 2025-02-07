dbee6d61e580accfc5ee758fbb6a056e4e41a1f0
brianbianchi <bianchibrianj@gmail.com>
2025-02-06 22:54:56 -0500

    static

--- core/settings.py
+++ core/settings.py
@@ -68,10 +68,11 @@ FIXTURE_DIRS = [os.path.join(BASE_DIR, "fixtures")]
 
 # Static files
 STATIC_URL = "/static/"
-if DEBUG:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+# collectstatic location
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 TEMPLATES = [
     {
@@ -157,13 +158,6 @@ USE_I18N = True
 USE_TZ = True
 
 SECURE_CROSS_ORIGIN_OPENER_POLICY = None
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = "static/"
-# collectstatic location
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
